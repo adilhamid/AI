1da7b00e6582a79e2b5f5e1264a1bc7beae287af
adilhamid <aaddil.hamid@gmail.com>
2017-12-01 17:38:40 -0600

    Updated the Problem 4 and added git igniore

---
+++ Project_2/Problem4.pl
@@ -0,0 +1,24 @@
+%Problem 4
+%Prime Factorization in Prolog
+factor(N,M):-
+    factorize(N,2,[],M).
+
+%Base Cases
+factorize(1,Count,M,M).
+
+%Main Function to call the factorization function
+factorize(N,Count,Acc, M):-
+    divisible(N,Count),Count =< N,
+    X is N/Count,
+    factorize(X,Count,[Count|Acc],M).
+
+factorize(N,Count,Acc,M):-
+    Count =< N,NewCount is Count+1,
+    factorize(N,NewCount,Acc,M).
+
+factorize(N,Count,Acc,M):-
+     Count>=N,
+     factorize(1,Count,Acc,M).
+
+divisible(N,X):-
+    M is N mod X, M=0.
