0ded5b5352b606f56930d375bcbd4c6a2a1d2763
adilhamid <aaddil.hamid@gmail.com>
2017-12-02 01:42:23 -0600

    Adding the solution to problem 6

---
+++ Project_2/Problem6.pl
@@ -0,0 +1,25 @@
+%Implementation of Sin_Zero
+threshold(Val):-
+   % write("THRESHOLD: "),writeln(Val),
+    Val >= -0.0001, Val =< 0.0001.
+
+update(X,Y):-
+    S is sin(X), C is cos(X),
+    D is S/C,
+    NX is X - D,
+   % writeln("Updating the Value mate: "),
+   % writeln(X), writeln(D), writeln(S), writeln(C), writeln(NX),
+    newtonM(NX,Y).
+
+thresholdCheck(X):-
+    Y is sin(X), threshold(Y).
+
+newtonM(X,Y):-
+    Y is X,
+    thresholdCheck(Y).
+
+newtonM(X,Y):-
+    update(X,Y).
+
+sin_zero(X,Result):-
+    newtonM(X,Result).
