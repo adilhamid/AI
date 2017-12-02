cac22def688993bc1408c66a4bbd9675bd2d69f4
adilhamid <aaddil.hamid@gmail.com>
2017-12-02 12:56:20 -0600

    Updated the code for Problem & SEND MORE MONEY

--- Project_2/Problem7.pl
+++ Project_2/Problem7.pl
@@ -17,8 +17,8 @@ sendMoreMoney:-
     digit(S), digit(E), digit(N), digit(D),digit(M), digit(O),digit(R), digit(N), digit(Y),
     M is 1+0, S>7,N is E+1, % Problem Statement Heuristics
     SEND is 1000*S+100*E+10*N+D,
-    MORE is 1000+100*O+10*R+E,
-    MONEY is 10000 + 1000*O + 100*N + 10*E + Y,
+    MORE is 1000*M+100*O+10*R+E,
+    MONEY is 10000*M + 1000*O + 100*N + 10*E + Y,
     MONEY is SEND + MORE,
     different([S,E,N,D,M,O,R,Y]),
     print([[S, E, N, D], [M, O, R, E], [M, O, N, E, Y]]).
